1d8c81c74bc67a48d8017cef1489aea953af8824
Vitalii Liashchenko <vitalik1996@gmail.com>
2021-07-31 15:15:41 +0300

    Create student form

--- hillel_django_project/students/views.py
+++ hillel_django_project/students/views.py
@@ -2,10 +2,12 @@
 from random import randrange
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from faker import Faker
 
 from .models import Student
+from .forms import StudentForm, StudentFormFromModel
 
 
 def hello(request):
@@ -46,3 +48,37 @@ def generate_students(request, student_number=100):
     students_list = Student.objects.all()
     output = [f"{student.first_name} {student.last_name}, {student.age}; \n" for student in students_list]
     return HttpResponse(output, content_type="text/plain")
+
+
+def create_student(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = StudentForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            Student.objects.create(**form.cleaned_data)
+            return HttpResponse('Student created!')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = StudentForm()
+
+    return render(request, 'student.html', {'form': form})
+
+
+def create_student_from_model(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = StudentFormFromModel(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            Student.objects.create(**form.cleaned_data)
+            return HttpResponse('Student created!')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = StudentFormFromModel()
+
+    return render(request, 'student_from_model.html', {'form': form})
