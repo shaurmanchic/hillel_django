595de83b2ffc73a9374f5e91a59030a61e1b4de3
Vitalii Liashchenko <vitalik1996@gmail.com>
2021-08-05 15:43:43 +0300

    Routing done

--- students/forms.py
+++ students/forms.py
@@ -3,13 +3,7 @@ from django import forms
 from .models import Student
 
 
-class StudentForm(forms.Form):
-    first_name = forms.CharField(label='Student\'s first name', max_length=200)
-    last_name = forms.CharField(label='Student\'s last name', max_length=200)
-    age = forms.IntegerField(label='Student\'s age')
-
-
 class StudentFormFromModel(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'age']
+        fields = ['id', 'first_name', 'last_name', 'age']
--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,4 @@
-"""hillel_django_project URL Configuration
+"""students URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,11 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
+from students.views import (
+    hello,
+    list_students,
+    get_student,
+    generate_students,
+    create_student,
+    edit_student,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('students.urls'))
+    path('hello/', hello),
+    path('students/', list_students, name='list-students'),
+    path('get_student/<int:student_id>', get_student, name='get-student'),
+    path('create_student', create_student),
+    path('generate_students/<int:student_number>', generate_students),
+    path('edit_student/<int:student_id>', edit_student, name='edit-student'),
 ]
